bd7181691f70c465ac5e2a2ebafb34c61c9ffa6d
Michael Schutter <michael_schutter_h29@hotmail.com>
2014-01-30 12:34:34 +0100

    derde commit

--- js/game.js
+++ js/game.js
@@ -21,6 +21,7 @@ window.onload = function(){
 	window.auto = new Image();
 	auto.src = "img/carrearend_small.png";
 
+	window.autoPos = new Object();
 
 //  posities van de auto
 	window.autoLinks = 0;
@@ -28,7 +29,7 @@ window.onload = function(){
 	window.autoRechts = 2;
 
 //  auto links x en y
-	window.autoLinksx = 110;
+	window.autoLinksx = 90;
 	window.autoLinksy = 290;
 
 //  auto midden x en y
@@ -36,7 +37,7 @@ window.onload = function(){
 	window.autoMiddeny = 290;
 
 //  auto rechts x en y
-	window.autoRechtsx = 450;
+	window.autoRechtsx = 500;
 	window.autoRechtsy = 290;
 
 
@@ -54,6 +55,7 @@ window.onload = function(){
 
 //  enemy tijd teller, elke zoveel tijdtellen een nieuw enemy
 	window.tijdTeller = 0;
+	window.seconden = 0;
 //  enemy sizes
 	window.enemyHeight = 100;
 	window.enemyWidth = 100;
@@ -85,9 +87,49 @@ window.onload = function(){
 	enemySoortArray.push(enemy_voetbal);
 	enemySoortArray.push(enemy_sms);
 
+//  enemy messages
+	
+	window.enemyMessage_1 = new Image(); 
+	enemyMessage_1.src = "img/enemy_whatsapp_message_1.png";
+
+	window.enemyMessage_2 = new Image(); 
+	enemyMessage_2.src = "img/enemy_livescore_1.png";
+
+	window.enemyMessage_3 = new Image(); 
+	enemyMessage_3.src = "img/enemy_livescore_2.png";
+
+	window.enemyMessage_4 = new Image(); 
+	enemyMessage_4.src = "img/enemy_livescore_3.png";
+
+	window.enemyMessage_5 = new Image(); 
+	enemyMessage_5.src = "img/enemy_livescore_4.png";
+
+	window.enemyMessage_6 = new Image(); 
+	enemyMessage_6.src = "img/enemy_livescore_6.png";
+
+	window.enemyMessage_7 = new Image(); 
+	enemyMessage_7.src = "img/enemy_livescore_7.png";	
+
+
+//  enemy message array 
+
+	window.currentMessage = 0;
+	window.messageY = 0;
+	window.enemyMessages = new Array();
+
+	enemyMessages.push(enemyMessage_1);
+	enemyMessages.push(enemyMessage_2);
+	enemyMessages.push(enemyMessage_3);
+	enemyMessages.push(enemyMessage_4);
+	enemyMessages.push(enemyMessage_5);
+	enemyMessages.push(enemyMessage_6);
+	enemyMessages.push(enemyMessage_7);
+
+
+//  vullen 	
 
 //  als toets word ingedrukt dan keyboardListener functie aanroepen
-	window.onkeyup = keyboardListener;
+	window.onkeydown = keyboardListener;
 
 //  canvas game achtergrond plaatje
 	window.bg_canvasGameBackgroundImage = new Image();
@@ -95,6 +137,8 @@ window.onload = function(){
 
 //  Setinvals
 	window.startFunctionsInterval;
+	window.tijdSecondenInterval;
+
 
 //  game status 
 //	0 = begin scherm
@@ -112,55 +156,102 @@ window.onload = function(){
 
 //  Teken startscherm
 	bg_canvasStartScreenImage.onload = function(){
-		//drawStartScreen();
-		//window.canvas.style.display = 'none';
-		startGame();
+		drawStartScreen();
+		window.canvas.style.display = 'none';
+		gameStatus = 0;
+		//startGame();
 	}
-
-
 
 } // einde window.onload
 
-
-// als er op play gedrukt wordt start de game en het drawen
-function startGame() {
-
-	// set startscherm op display none;
+// als je af ben en opnieuw begint alles op 0 zetten en weer starten
+function restartGame() {
+	
 	startCanvas.style.display = 'none';
 	canvas.style.display = 'inline';
-	startFunctionsInterval = setInterval(gameFunctions, 10);
-	gameStatus = 1;
 
-} // einde startgame()
+	gameStatus = 1;
+	enemyArray.length = 0;
+	currentMessage = 0;
+	autoPositie = 1;
+	tijdTeller = 0;
+	gameProgressie = 0;	
+	messageY = 0;
+	seconden = 0;
+	startFunctionsInterval = setInterval(gameFunctions, 10);
+	tijdSecondenInterval = setInterval(everySecond, 1000);
+
+}
+
+function endGame() {
+
+	gameStatus = 3;
+	window.clearInterval(startFunctionsInterval);
+	window.clearInterval(tijdSecondenInterval);
+
+	drawEindScherm();
+
+}
 
 function gameFunctions() {
 	//console.log('in de gameFunctions()');
-	reDraw();
-	collisionDetection();
+	if(gameStatus == 1) {
+		reDraw();
+		collisionDetection();
+	}
 }
 
 function resumeGame() {
 	
 }
 
+function collisionDetection() {
+
+
+	for(var i = 0; i < enemyArray.length; i++ ) {
+
+		if( enemyArray[i].levend) {
+			//console.log('auto xpos+61 =  ' + (autoPos.xPos + 61) +  ' enemy xmax = ' + (enemyArray.xPos + (enemyArray[i].scaleFactor * enemyWidth)) );
+			//console.log('scale factor = ' + enemyArray[i].scaleFactor + '   enemy width = ' + enemyWidth);
+
+			if (enemyArray[i].yPos < autoPos.yPos + 30) {
+			// if als er botsing is
+				if(  enemyArray[i].yPos + enemyArray[i].scaleFactor*enemyHeight  >= autoPos.yPos  && enemyArray[i].xPos <= (autoPos.xPos + 61) && enemyArray[i].xPos + (enemyArray[i].scaleFactor * enemyWidth) >= (autoPos.xPos + 61)  ) {
+					gameStatus = 3;
+					drawEindScherm();
+				} // als er botsing is geweest
+			}
+
+		} // einde if levend
+
+	} // einde for loop
+
+} // einde collisiondetection()
 
 function keyboardListener(e) {
 	// 37 links / 38 omhoog / 39 rechts / 40 beneden / 80 P / 27 ESC / t 84 / y 89
 
 	var code = e.keyCode ? e.keyCode : e.which;
-    if (code === 37) { //up key
-        if( autoPositie === 1 || autoPositie === 2) {
-        	if(gameStatus == 1) {
-        		autoPositie--;	
-        	}
-        } 
-    } else if (code === 39) { //down key
-        if( autoPositie === 0 || autoPositie === 1) {
-        	if(gameStatus == 1) {
+
+	if(gameStatus == 1) {
+	    if (code === 37) { //key left
+	        if(autoPositie === 0) {
+
+	        	endGame();
+	        }
+	        if( autoPositie === 1 || autoPositie === 2) {
+	    		autoPositie--;	
+	        } 
+
+	    } else if (code === 39) { //key right
+	        if(autoPositie === 2) {
+	        	endGame();
+	        }
+	        if( autoPositie === 0 || autoPositie === 1) {
         		autoPositie++;	
         	}
-        } 
-    }
+	   	}
+   	}
 
     // P knop gedrukt
     if (code == 80) {
@@ -176,28 +267,33 @@ function keyboardListener(e) {
 
     // ESC knop gedrukt
     if(code == 27) {
-    	gameStatus = 2;
+    	endGame();
+    	restartGame();
     }
 
+    // T knop ingedrukt
     if(code == 84) {
-    	createEnemy();
+    	if(gameStatus == 3) {
+    	createEnemy(); }
     	//console.log('createEnemy()');
     }
+    // Y knop ingedrukt
     if(code == 89) {
 
-    	console.log(enemyArray[0].row);
+    	console.log(auto);
+
     }
-};
+} // einde 
 
 
 // teken functies
 function reDraw() {
 
-	if(gameStatus == 1) {
-		drawGameBackground();
-		drawCar();
-		drawEnemies();
-	}
+	canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+	drawGameBackground();
+	drawCar();
+	drawEnemies();
+	drawMessages();
 
 } // einde redraw()
 
@@ -206,114 +302,93 @@ function drawCar() {
 
 	// auto links
 	if (autoPositie == 0) {
-
-		canvasContext.drawImage(auto, autoLinksx, autoLinksy);
+		autoPos.xPos = autoLinksx;
+		autoPos.yPos = autoLinksy;
 
 	} // auto midden
 	else if (autoPositie == 1) {
-
-		canvasContext.drawImage(auto, autoMiddenx, autoMiddeny);
+		autoPos.xPos = autoMiddenx;
+		autoPos.yPos = autoMiddeny;
 
 	} // auto rechts
 	else if (autoPositie == 2) {
-
-		canvasContext.drawImage(auto, autoRechtsx, autoRechtsy);
+		autoPos.xPos = autoRechtsx;
+		autoPos.yPos = autoRechtsy;
 	}
-
+	
+	canvasContext.drawImage(auto, autoPos.xPos, autoPos.yPos);
 } // einde drawCar();
 
 // maakt nieuwe enemy
-function createEnemy() {
+function createEnemy(row) {
 
 	// xPos = x positie / yPos = y positie / scaleFactor = schaalfactor(grootte) / row = welke rijbaan enemy zit / status = status (0 leven, 1 dood/weg)
 	enemy = new Object();
 
-	enemy.row =  Math.floor(Math.random()* 3 );
+	enemy.row = row;
+	//enemy.row =  Math.floor(Math.random()* 3 );
 	//enemy.row = 1;
 
-
-	console.log('enemy.row in create enenmy ' + enemy.row);
-	
 	// als het straks random is
 	enemy.soort = Math.floor( (Math.random()*5) );
 	//enemy.soort = 0; // facebook nu
 
 	if(enemy.row == 0) {
 		enemy.xPos = 150;
-		enemy.yPos = 90; 
+		enemy.yPos = 0; 
 
 	} else if (enemy.row == 1){
 		enemy.xPos = 350;
-		enemy.yPos = 90; 
+		enemy.yPos = 0; 
 
 	} else if (enemy.row == 2) {
 		enemy.xPos = 546;
-		enemy.yPos = 90; 
+		enemy.yPos = 0; 
 
 	}
 
-	enemy.scaleFactor = 0.4;
+	enemy.scaleFactor = 1;
 	enemy.levend = true;
 
 	enemyArray.push(enemy);
 
 }
 
-
 // tekent de enemies
 function drawEnemies() {
 	
 	tijdTeller++;
 
-	if( (tijdTeller % 200) == 0) {
-
-		createEnemy();
-	}
-	if(gameProgressie == 0) {
-
 		
-		for(var i = 0; i < enemyArray.length; i++ ) {
-			//console.log('in de drawEnemies() for loop');
-			// img, x, y, width, height
-			//console.log( 'enemyArray .type ' + enemyArray[i].type  );//enemySoortArray[enemyArray[i].type].src);
+	for(var i = 0; i < enemyArray.length; i++ ) {
+		//console.log('in de drawEnemies() for loop');
+		// img, x, y, width, height
+		//console.log( 'enemyArray .type ' + enemyArray[i].type  );//enemySoortArray[enemyArray[i].type].src);
+		
+		// check enemy.levend = true
+		if(enemyArray[i].levend) {
+
+/*			if(enemyArray[i].scaleFactor <= 1 && (tijdTeller % 2) == 0) {
+
+				enemyArray[i].scaleFactor += 0.01;
+			}*/
+			if(enemyArray[i].yPos <= 400 ) {
+				//volgPad(i);
+				enemyArray[i].yPos+=1;
+			} // einde if ypos >= 400
+			else {
+				enemyArray[i].levend = false;
+				//enemyArray.splice(i, 1);
+			}
+
+			canvasContext.drawImage( enemySoortArray[enemyArray[i].soort], enemyArray[i].xPos - (enemyWidth*enemyArray[i].scaleFactor / 2), enemyArray[i].yPos, enemyWidth*enemyArray[i].scaleFactor, enemyHeight*enemyArray[i].scaleFactor );
+
+		} // einde if enemy is levend
 			
-			// check enemy.levend = true
-			if(enemyArray[i].levend) {
-
-
-				if(enemyArray[i].scaleFactor <= 1 && (tijdTeller % 2) == 0) {
-
-					enemyArray[i].scaleFactor += 0.01;
-				}
-				if(enemyArray[i].yPos <= 400 ) {
-					//volgPad(i);
-					enemyArray[i].yPos+=1;
-				} // einde if ypos >= 400
-				else {
-					enemyArray[i].levend = false;
-				}
-
-				canvasContext.drawImage( enemySoortArray[enemyArray[i].soort], enemyArray[i].xPos - (enemyWidth*enemyArray[i].scaleFactor / 2), enemyArray[i].yPos, enemyWidth*enemyArray[i].scaleFactor, enemyHeight*enemyArray[i].scaleFactor );
-
-			} // einde if enemy is levend
-				
-		} // einde for loop;
-
-	} // einde if gameprogressie = 0
-	// 
-	else if (gameProgressie == 1) {
-
-
-	} // einde if gameProgessie == 1
-	else if (gameProgessie == 2) {
-
-
-	} // einde if gameProgressie == 3 
+	} // einde for loop;
 
 
 } // einde drawEnemies
-
-
 
 
 // tekent het startscherm
@@ -327,7 +402,7 @@ function drawStartScreen() {
 // tekent achtergrond
 function drawGameBackground() {
 	//console.log('in de drawGameBackground()');
-	canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+
 	canvasContext.drawImage(bg_canvasGameBackgroundImage, 0, 0);
 
 } // einde drawgamebackground();
@@ -342,7 +417,119 @@ function drawPauzeScherm() {
 
 } // einde drawpauzescherm()
 
+// tekent pauze scherm 
+function drawEindScherm() {
 
+	if(gameStatus == 3) {
+		canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+		canvasContext.drawImage(bg_eindSchermBackgroundImage, 0, 0);
+		canvasContext.font="16px Arial";
+		canvasContext.fillText("Je hebt " + seconden + " seconden gereden",44,210);
+	}
+
+} // einde drawpauzescherm()
+
+// tekent de berichtjes
+
+function drawMessages() {
+
+	if(gameStatus == 1) {
+
+		if( seconden >= 3 && seconden <= 6 || 
+			seconden >= 8 && seconden <= 12 || 
+			seconden >= 14 && seconden <= 16 ||
+			seconden >= 18 && seconden <= 20 ||
+			seconden >= 22 && seconden <= 23 ||
+			seconden >= 25 && seconden <= 28 ||
+			seconden >= 30 && seconden <= 31 ||
+			seconden >= 33 && seconden <= 36
+
+			) {
+			
+			canvasContext.drawImage(enemyMessages[currentMessage], 57, messageY);
+
+
+		} // einde if secopnden
+
+	} // einde if gamestatus == 1 
+
+}
+
+function everySecond() {
+
+	spawnEnemies();
+	secondenPlusPlus();
+}
+
+function secondenPlusPlus() {
+
+	if(gameStatus == 1) {
+
+		if (seconden == 6 || seconden == 12 || seconden == 16 || seconden == 20 || seconden == 23 || seconden == 28 || seconden == 31 || seconden == 36) {
+			currentMessage++;
+		}
+
+
+		if (seconden == 10 || seconden == 20) {
+
+			messageY += 15;
+		}
+
+
+		seconden++;
+		console.log('seconde asdfsaf: ' + seconden + '  GANMEPROGRESSIE : ' + gameProgressie + '   enemy array length' +  enemyArray.length  );
+
+
+	}
+
+}
+
+function spawnEnemies() {
+
+	// draw 0 en 1
+	if (seconden == 6 || seconden == 16 || seconden == 26 || seconden == 34) {
+
+		createEnemy(0);
+		createEnemy(1);
+	}
+
+
+	// draw 1 en 2
+	if (seconden == 2 || seconden == 12 || seconden == 20 || seconden == 24 || seconden == 30 ) {
+
+		createEnemy(1);
+		createEnemy(2);
+	}
+
+	// draw 0 en 2
+	if (seconden == 4 || seconden == 10 || seconden == 14 || seconden == 22 || seconden == 32) {
+
+		createEnemy(2);
+		createEnemy(0);
+	}
+
+	// draw 0
+
+	if(seconden == 0) {
+
+		createEnemy(0);
+	}
+
+	// draw 1
+	if (seconden == 8 || seconden == 28) {
+
+		createEnemy(1);
+	}
+
+	// draw 2
+
+	if (seconden == 18) {
+
+		createEnemy(2);
+	}
+
+
+}
 
 
 
